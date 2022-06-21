70cfc4f5af0ce59e3b59555d7a3b55987d3e6ef4
Juan G Lopez <nahujg@gmail.com>
2022-06-21 16:37:56 +0200

    iteración 3 terminada

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ class SortedList {
     this.items = [];
     this.length = this.items.length;
   }
-
-  add(item) {}
-
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
   max() {}
 
